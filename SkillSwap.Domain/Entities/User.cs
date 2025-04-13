d358ef05f425314dfa588de27f295a69ebfa91c2
Sergiu <Sergiusay06@gmail.com>
2025-04-13 20:33:10 +0300

    Add Entityes

---
+++ SkillSwap.Domain/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillSwap.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string HashPassword { get; set; }
+        public Guid OfferdSkillId { get; set;}
+        public Skill OfferdSkill { get; set; }
+        public Guid WantedSkillId { get; set; }
+        public Skill WantedSkill { get; set; }
+    }
+}
